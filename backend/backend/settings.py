6d84740fb66b7c26667d5eb640a028ba02af9689
Виктор Золотарев <vik.zolotarev@mail.ru>
2026-04-29 22:12:40 +0300

    New Taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['taskishow.ru', 'www.taskishow.ru', '127.0.0.1', 'localhost', '158.160.247.104']
 
 
 # Application definition
